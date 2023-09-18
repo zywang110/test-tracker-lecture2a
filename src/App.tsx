c344f6252468a0d9c8d8b2a1c6c6f1dad193edb0
sillyfunnypedro <sillyfunnypedro@gmail.com>
2023-09-18 16:51:19 -0700

    update complete

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,24 @@ import TasksComponent from './TasksComponent';
 
 const tasks = new Tasks();
 tasks.addTask('learn React', 'learn this new thing', false, 0);
-tasks.addTask('learn React2', 'learn this new thing2', false, 1);
+tasks.addTask('learn React2', 'learn this new thing2', false, 0);
 
 function App() {
 
   const [tasksCompleted, setTasksCompleted] = React.useState(0);
   const [taskCount, setTaskCount] = React.useState(0);
+
+  function incrementTime(taskName: string) {
+    // get the task
+    let task = tasks.getTask(taskName);
+    // increment the time
+    task!.hours += 1;
+
+
+    tasks.updateTask(task!.name, task!.description, task!.status, task!.hours);
+    setTaskCount(taskCount + 1);
+    //console.log(taskCount);
+  }
 
   React.useEffect(() => {
     setTaskCount(tasks.getTasks().keys.length);
@@ -24,8 +36,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {tasksCompleted}
-        <TasksComponent tasks={tasks} />
+        Total Hours:{taskCount}
+        <TasksComponent tasks={tasks} incrementTime={incrementTime} />
       </header>
 
     </div>
--- src/TasksComponent.tsx
+++ src/TasksComponent.tsx
@@ -8,11 +8,27 @@ import Tasks from './Tasks';
 
 interface TasksProps {
     tasks: Tasks;
+    incrementTime: (taskName: string) => void;
 }
 
-// Display the tasks that are in the list.  the name and the status of the task.
 
-const TasksComponent: React.FC<TasksProps> = ({ tasks }) => {
+
+// Display the tasks that are in the list.  the name and the status of the task.
+// have a button to add one hour to the task.  the call back will be in the parent component.
+const TasksComponent: React.FC<TasksProps> = ({ tasks, incrementTime }) => {
+
+    // callback for the button to add an hour to the task.
+    // the parameter is the event
+    // the event.target is the button
+
+    function addHour(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        console.log(event.target);
+        let taskName = event.currentTarget.id;
+        console.log(taskName);
+        incrementTime(taskName);
+
+    }
+
     return (
         <div>
             <h1>Tasks</h1>
@@ -20,6 +36,7 @@ const TasksComponent: React.FC<TasksProps> = ({ tasks }) => {
                 {tasks.getTasks().map((task) => (
                     <li key={task.name}>
                         {task.name} -{task.hours} - {task.status ? 'Complete' : 'Incomplete'}
+                        <button id={task.name} onClick={addHour}>{"Add One Hour"}</button>
                     </li>
 
                 ))}
